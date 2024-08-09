7b441ac8ba5ebf55c30784481500598d710ffff7
Dave Arnold <dave@roknsound.com>
2024-08-09 09:53:24 -0700

    chore: Update main.tf to add unattended flag and set work directory for GitHub Actions runner registration

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ locals {
     ["./config.sh --url ${var.github_base_url}/${var.github_owner}/${repo}"],
     ["--token ${lookup(data.github_actions_registration_token.token, repo).token}"],
     ["--name ${repo}"],
+    ["--unattended"],
+    ["--work ${repo}"],
     length(var.runner_labels) > 0 ? ["--labels ${join(",", var.runner_labels)}"] : [],
     var.runner_group != null ? ["--runnergroup ${github_actions_runner_group.rg[0].id}"] : []
   )) }
